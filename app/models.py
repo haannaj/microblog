c64da7bb989da82a3a7da797632763f8df4ab0af
haannaj <73937750+haannaj@users.noreply.github.com>
2022-11-10 18:25:58 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,6 +9,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     """
     Represetns a system User
@@ -37,6 +43,12 @@ class User(UserMixin, db.Model):
         current_app.logger.debug(f"Checking password {password}")
         return check_password_hash(self.password_hash, password)
 
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+        
     @staticmethod
     @login.user_loader
     def load_user(id_):
